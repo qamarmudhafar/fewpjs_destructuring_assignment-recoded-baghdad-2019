92e0e0e3eab366a6958adc77586524c64c0bd0c6
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-15 07:00:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const farmAnimals = 'cow horse sheep pig chicken'
+const[moo,neigh ,baa,oink,cluck]='cow horse sheep pig chicken'.split(' ');
+const[Bessie, , Dolly, Babe, Little]=farmAnimals.split(' ')
+const [black_and_white,,black,pink,] = farmAnimals.split(' ');
+
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+const[red,orange,yellow,green,blue,indigo,violet]=colors;
+const[r,o,y,g,b, ,v]=colors;
+const[ , , , , ,indg, ] = colors;
+
 
 const muppet = {
   name: 'Kermit',
@@ -9,6 +17,8 @@ const muppet = {
   job: 'Host of The Muppet Show',
   partner: 'Miss Piggy'
 };
+const {name,color,song,job,partner} = muppet;
+
 
 const k_muppet = {
   k_name: 'Kermit',
@@ -24,6 +34,8 @@ const k_muppet = {
   k_job: 'Host of The Muppet Show',
   k_partner: 'Miss Piggy'
 };
+const { song_2, song_4} = k_muppet.k_album.the_muppet_movie;
+const {k_color, k_job, k_partner } = k_muppet;
 
 // Strings
 
